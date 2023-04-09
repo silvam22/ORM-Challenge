fe17b58050dc9fab136017666272a3e8fa639980
miriam <miriamsilvaaa484@gmail.com>
2023-04-09 12:43:23 -0700

    fixed some bug problems

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,64 +1,101 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
+// Get all categories
 router.get('/', async (req, res) => {
-try {
-  const categoriesData = await Category.findAll({ include: [{ model: Product }] });
-  res.status(200).json(categoriesData);
-} catch (err){
-  console.error(err);
-  res.status(500).json(err);
-}
-
+  try {
+    const categories = await Category.findAll();
+    res.json(categories);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Unable to get categories.' });
+  }
 });
 
+// Get a category by id
 router.get('/:id', async (req, res) => {
   try {
-    const categoryData = await Category.findOne({
-      where: { id: req.params.id },
-      include: [{ model: Product }]
+    const category = await Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: {
+        model: Product,
+        attributes: ['category_id']
+      }
     });
-    if (!categoryData) {
-      res.status(404).json({ message: 'No category to be found' });
+
+    if (!category) {
+      res.status(404).json({ error: 'Category not found.' });
       return;
     }
-    res.status(200).json(categoryData);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json(err);
+
+    res.json(category);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Unable to get category.' });
   }
 });
 
+// Create a new category
 router.post('/', async (req, res) => {
   try {
-    const newCategory = await Category.create(req.body);
-    res.status(200).json(newCategory);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json(err);
+    const newCategory = await Category.create({
+      category_name: req.body.category_name
+    });
+
+    res.json(newCategory);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Unable to create category.' });
   }
 });
 
+// Update a category by id
 router.put('/:id', async (req, res) => {
   try {
-  const improveCategory = await Category.updatedOne({_id: req.params.id }, req.body );
-  res.status(200).json(improveCategory);
-} catch (err) {
-  console.error(err);
-  res.status(500).json(err);
-}
+    const [rowsUpdated] = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+
+    if (!rowsUpdated) {
+      res.status(404).json({ error: 'Category not found.' });
+      return;
+    }
+
+    res.json({ message: 'Category updated successfully.' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Unable to update category.' });
+  }
 });
 
+// Delete a category by id
 router.delete('/:id', async (req, res) => {
-try {
-  const goneCategory = await Category.deleteOne({_id: req.params.id });
-  res.status(200).json(goneCategory);
-} catch (err) {
-  console.error(err);
-  res.status(500).json(err);
-}
+  try {
+    const rowsDeleted = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!rowsDeleted) {
+      res.status(404).json({ error: 'Category not found.' });
+      return;
+    }
+
+    res.json({ message: 'Category deleted successfully.' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Unable to delete category.' });
+  }
 });
 
 module.exports = router;
